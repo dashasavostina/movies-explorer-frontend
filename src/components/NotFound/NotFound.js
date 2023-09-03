8631dfd071ab46f18d779f9b27bed29e5c237bfa
Dasha Savostina <moskvichka123@yandex.ru>
2023-09-03 16:46:17 +0300

    внесение правок после 1 итерации

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,4 +1,4 @@
-import { useHistory, Link } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory} from "react-router-dom/cjs/react-router-dom.min";
 import "./NotFound.css";
 
 export default function NotFound() {
@@ -8,7 +8,7 @@ export default function NotFound() {
     <section className="not-found">
       <p className="not-found__status">404</p>
       <h3 className="not-found__title">Страница не найдена</h3>
-      <button className="not-found__button" to="" onClick={() => history.goBack()}>
+      <button className="not-found__button" onClick={() => history.push(-1)}>
         Назад
       </button>
     </section>
